156abc21a8667565ff3de759b134cf049c073c7d
rahulsingh237 <singh.rahul220499@gmail.com>
2020-05-18 02:07:09 +0530

    4th commit

---
+++ GUI.py
@@ -0,0 +1,180 @@
+from tkinter import *
+from smartcalculator import calci
+
+root = Tk()
+# Icon
+root.wm_iconbitmap('cal.ico')
+# Width  x  Height
+root.geometry("644x800")
+
+# Fix window size
+root.minsize(479,350)
+root.maxsize(479,350)
+
+# Title
+root.title("SmartCalculator")
+
+
+
+# Display 
+scvalue = StringVar()
+scvalue.set("")
+screen = Entry(root, textvar=scvalue, font="lucida 40 bold") 
+screen.pack(fill=X)
+             
+
+# Function
+def click(event):
+    global scvalue
+    text = event.widget.cget("text")
+    if text == "Calculate":
+        if scvalue.get().isdigit():
+            value = int(scvalue.get())
+        else:
+            try:
+                value = eval(screen.get())
+            except Exception as e:
+                value = "Error"   
+
+        scvalue.set(value)
+        print(type(text))
+        screen.update()    
+   
+    elif text == "DEL": #scvalue=text
+         try:
+            fullstring = scvalue.get()
+            newstring=fullstring[:-1]
+            # we are replacing the last string item[-1] with blank or ""
+            # String slicing method
+            
+            scvalue.set(newstring)
+
+            # print(newstring)
+            screen.update()
+         except Exception as e:
+            print(e)
+             
+    elif text == "voice":
+        text = str(calci())
+        scvalue.set(scvalue.get() + text)
+        screen.update()
+    elif text == "All Clear":
+        scvalue.set("")
+        screen.update()
+    elif text == "OFF":
+        sys.exit()
+    else:
+        scvalue.set(scvalue.get() + text)
+        screen.update()
+
+
+
+#voice
+"""self.loadimage = root.PhotoImage(file="voice.png")
+self.roundedbutton = root.Button(self, image=self.loadimage)
+self.roundedbutton["bg"] = "white"
+self.roundedbutton["border"] = "0"
+self.roundedbutton.pack(side="top")"""
+
+# Frame
+mFrame = Frame(root)
+mFrame.pack(side=TOP, fill=X)
+
+
+vframe=Frame(mFrame,relief=GROOVE)
+vframe.pack(side=TOP)
+b=Button(vframe,text="voice",width=20,height=3)
+b.grid(row=0,column=0)
+b.bind("<Button-1>",click)
+b=Button(vframe,text="All Clear",width=20,height=3)
+b.grid(row=0,column=1)
+b.bind("<Button-1>",click)
+b=Button(vframe,text="OFF",width=20,height=3)
+b.grid(row=0,column=2,padx=15)
+b.bind("<Button-1>",click)
+
+topFrame = Frame(root)
+topFrame.pack(side=TOP, fill=X)
+
+
+numframe=Frame(topFrame,relief=RAISED)
+numframe.pack(side=LEFT,fill=X,padx=30)
+b = Button(numframe,text="7",width=10,height=3)
+b.grid(row=0,column=0)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="8",width=10,height=3)
+b.grid(row=0,column=1)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="9",width=10,height=3)
+b.grid(row=0,column=2)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="4",width=10,height=3)
+b.grid(row=1,column=0)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="5",width=10,height=3)
+b.grid(row=1,column=1)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="6",width=10,height=3)
+b.grid(row=1,column=2)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="1",width=10,height=3)
+b.grid(row=2,column=0)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="2",width=10,height=3)
+b.grid(row=2,column=1)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="3",width=10,height=3)
+b.grid(row=2,column=2)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="00",width=10,height=3)
+b.grid(row=3,column=0)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text="0",width=10,height=3)
+b.grid(row=3,column=1)
+b.bind("<Button-1>",click)
+
+b=Button(numframe,text=".",width=10,height=3)
+b.grid(row=3,column=2)
+b.bind("<Button-1>",click)
+
+
+opframe=Frame(topFrame,relief=GROOVE)
+opframe.pack(side=TOP,fill=X,padx=10)
+b=Button(opframe,text="DEL",width=10,height=3)
+b.grid(row=0,column=0)
+b.bind("<Button-1>",click)
+b=Button(opframe,text="%",width=10,height=3)
+b.grid(row=0,column=1)
+b.bind("<Button-1>",click)
+b=Button(opframe,text="+",width=10,height=3)
+b.grid(row=1,column=0)
+b.bind("<Button-1>",click)
+b=Button(opframe,text="-",width=10,height=3)
+b.grid(row=1,column=1)
+b.bind("<Button-1>",click)
+b=Button(opframe,text="*",width=10,height=3)
+b.grid(row=2,column=0)
+b.bind("<Button-1>",click)
+b=Button(opframe,text="/",width=10,height=3)
+b.grid(row=2,column=1)
+b.bind("<Button-1>",click)
+
+
+
+cframe=Frame(topFrame,relief=GROOVE)
+cframe.pack(side=LEFT,padx=15)
+b=Button(cframe,text="Calculate",width=20,height=3)
+b.pack()
+b.bind("<Button-1>",click)
+
+root.mainloop()
--- smartcalculator.py
+++ smartcalculator.py
@@ -3,142 +3,168 @@ from speak import male
 from speech import rect
 
 
-# responses that calculator will give 
-response=['Welcome to smart calculator','My name is Cavill', 
-		'I am glad that I was able to help you','Sorry ,this is beyond my ability'] 
+# responses that calculator will give
+response = ['Welcome to smart calculator', 'My name is Cavill',
+            'I am glad that I was able to help you', 'Sorry ,this is beyond my ability']
 
-# fetching tokens from the text command 
-def operations_from_text(text): 
-	l=[] 
-	for t in text.split(' '): 
-		try: 
-			l.append(float(t)) 
-		except ValueError: 
-			pass
-	return l 
-
-# calculating LCM 
-def lcm(a,b): 
-	L=a if a>b else b 
-	while L<=a*b: 
-		if L%a==0 and L%b==0: 
-			return L 
-		L+=1
-
-# calculating HCF 
-def hcf(a,b): 
-	H=a if a<b else b 
-	while H>=1: 
-		if a%H==0 and b%H==0: 
-			return H 
-		H-=1
-
-# Addition 
-def add(a,b): 
-	return a+b 
-
-# Subtraction 
-def sub(a,b): 
-	return a-b 
-
-# Multiplication 
-def mul(a,b): 
-	return a*b 
-
-# Division 
-def div(a,b): 
-	return a/b 
-
-# Remainder 
-def mod(a,b): 
-	return a%b 
-
-# Response to command 
-# printing - "I am glad that I was able to help you" on exit 
-def end(): 
-	male('I am glad that I was able to help you')
-	print(response[2])
-	input('press enter key to exit.')
-	male('press enter key to exit.')
-
-	exit() 
-
-def myname(): 
-	
-	print(response[1])
-	male('My name is Cavill')
-def sorry(): 
-	
-	print(response[3])
-	male('Sorry ,this is beyond my ability')
-
-# Operations - performed on the basis of text tokens 
-operations={'ADD':add,
-            'PLUS':add,
-            'SUM':add,
-            'ADDITION':add, 
-			'SUB':sub,
-            'SUBTRACT':sub, 
-            'MINUS':sub, 
-			'DIFFERENCE':sub,
-            'LCM':lcm,
-            'HCF':hcf, 
-			'PRODUCT':mul, 
-            'MULTIPLY':mul,
-            'MULTIPLICATION':mul, 
-			'DIVIDE':div,
-			'DIVISION':div,
-            'MOD':mod,
-            'REMANDER':mod,
-            'MODULAS':mod} 
+# fetching tokens from the text command
 
 
-# commands 
-commands={'NAME':myname,'EXIT':end,'QUIT':end,'END':end,'CLOSE':end} 
-		
-print('--------------'+response[0]+'------------') 
+def operations_from_text(text):
+    l = []
+    for t in text.split(' '):
+        try:
+            l.append(float(t))
+        except ValueError:
+            pass
+    return l
+
+# calculating LCM
+
+
+def lcm(a, b):
+    L = a if a > b else b
+    while L <= a*b:
+        if L % a == 0 and L % b == 0:
+            return L
+        L += 1
+
+# calculating HCF
+
+
+def hcf(a, b):
+    H = a if a < b else b
+    while H >= 1:
+        if a % H == 0 and b % H == 0:
+            return H
+        H -= 1
+
+# Addition
+
+
+def add(a, b):
+    return a+b
+
+# Subtraction
+
+
+def sub(a, b):
+    return a-b
+
+# Multiplication
+
+
+def mul(a, b):
+    return a*b
+
+# Division
+
+
+def div(a, b):
+    return a/b
+
+# Remainder
+
+
+def mod(a, b):
+    return a % b
+
+# Response to command
+# printing - "I am glad that I was able to help you" on exit
+
+
+def end():
+    male('I am glad that I was able to help you')
+    print(response[2])
+    input('press enter key to exit.')
+    male('press enter key to exit.')
+
+    exit()
+
+
+def myname():
+
+    print(response[1])
+    male('My name is Cavill')
+
+
+def sorry():
+
+    print(response[3])
+    male('Sorry ,this is beyond my ability')
+
+
+# Operations - performed on the basis of text tokens
+operations = {'ADD': add,
+              'PLUS': add,
+              'SUM': add,
+              'ADDITION': add,
+              'SUB': sub,
+              'SUBTRACT': sub,
+              'MINUS': sub,
+              'DIFFERENCE': sub,
+              'LCM': lcm,
+              'HCF': hcf,
+              'PRODUCT': mul,
+              'MULTIPLY': mul,
+              'MULTIPLICATION': mul,
+              'DIVIDE': div,
+              'DIVISION': div,
+              'MOD': mod,
+              'REMANDER': mod,
+              'MODULAS': mod}
+
+
+# commands
+commands = {'NAME': myname, 'EXIT': end, 'QUIT': end, 'END': end, 'CLOSE': end}
+
+print('--------------'+response[0]+'------------')
 male('Welcome to smart calculator')
-print('--------------'+response[1]+'--------------------') 
+print('--------------'+response[1]+'--------------------')
 male('My name is Cavill')
 
 
-while True: 
-	print('enter your queries: ')
-	male('enter your queries:') 
-	text=rect()
-	
+def calci():
+    while True:
+        print('enter your queries: ')
+        male('enter your queries:')
+        text = rect()
+        r = 0
+        for word in text.split(' '):
+            if word.upper() in operations.keys():
+                try:
+                    l = operations_from_text(text)
+                    q = str(operations[word.upper()].__name__)
+                    if q == 'mul':
+                        r = 1
+                        for i in range(len(l)):
+                            r = operations[word.upper()](r, l[i])
+                    elif q == 'add':
+                        for i in range(len(l)):
+                            r = operations[word.upper()](r, l[i])
+                    elif q == 'div' or 'lcm' or 'hcf' or 'sub':
+                        r = float(l[0])
+                        for i in range(1, len(l)):
+                            r = operations[word.upper()](r, l[i])
+                    elif q == 'mod':
+                            r = operations[word.upper()](l[0], l[1])
+                    print(r)
+                    male(r)
+                    return(r)
+                    break
+                except:
+                    print('something went wrong going please enter again !!')
+                    male('something went wrong going please enter again !!')
+                    r='something went wrong going please enter again !!'
+                    break
+                finally:
+                    break
+            elif word.upper() in commands.keys():
+                commands[word.upper()]()
+                break
+        else:
+            sorry()
+        return(r)       
 
-	r=0
-	for word in text.split(' '): 
-		if word.upper() in operations.keys(): 
-			try: 
-				l = operations_from_text(text)
-				q = str(operations[word.upper()].__name__)
-				if q == 'mul':
-					r=1
-					for i in range(len(l)):
-						r = operations[word.upper()] (r,l[i]) 
-				elif q == 'add':
-					for i in range(len(l)):
-						r = operations[word.upper()] (r,l[i]) 
-				elif q == 'div' or 'lcm' or 'hcf' or 'sub':	
-					r=float(l[0])
-					for i in range(1,len(l)):
-						r = operations[word.upper()] (r,l[i])
-				elif q == 'mod':
-					r = operations[word.upper()] (l[0],l[1])
-				print(r)
 
-				male(r)						
-				 
-				r=0
-			except: 
-				print('something went wrong going please enter again !!') 
-				male('something went wrong going please enter again !!')
-			finally: 
-					break
-		elif word.upper() in commands.keys(): 
-					commands[word.upper()]() 
-					break
-	else:		 
-		sorry() 
+    
